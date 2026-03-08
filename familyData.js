5c57cdbd8a33f1a5a2562453ba3eda67a01db15e
Mia S <mia0613@gmail.com>
2026-03-08 17:42:06 -0500

    Update familyData.js

--- familyData.js
+++ familyData.js
@@ -15,7 +15,7 @@ const family =
   },
   children: [
     {
-      name: "Maria",
+      name: "Anna",
       gender: "female",
       image: "images/p3.jpg",
       info: "Born 1965",
@@ -29,13 +29,112 @@ const family =
 
       children: [
         {
-          name: "Elena",
+          name: "Wanda",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+          spouse: {
+            name: "Carlos",
+            gender: "male",
+            image: "images/p4.jpg",
+            info: "Architect"
+          },          
+          children: [    
+          {
+          name: "Joan",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+            spouse: {
+            name: "Bill",
+            gender: "male",
+            image: "images/p4.jpg",
+            info: "Architect" },
+          children: [      {
+          name: "Valerie",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+          children: [      {
+              name: "Mia",
+              gender: "female",
+              image: "images/p5.jpg",
+              info: "Born 1990",
+              children: []
+            },       {
+              name: "Ben",
+              gender: "female",
+              image: "images/p5.jpg",
+              info: "Born 1990",
+              children: []
+            }]
+        },
+      {
+          name: "Mike",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+            spouse: {
+            name: "Yuko",
+            gender: "male",
+            image: "images/p2.jpg",
+            info: "Born 1938 • Military leader"
+          },
+          
+        }, 
+              {
+          name: "Peter",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+            spouse: {
+            name: "Mary",
+            gender: "male",
+            image: "images/p2.jpg",
+            info: "Born 1938 • Military leader"
+          },
+          children: []
+        }     
+      ]
+        },
+      {
+          name: "Sue",
           gender: "female",
           image: "images/p5.jpg",
           info: "Born 1990",
           children: []
-        }
+        },
+{
+          name: "Dan",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+          children: []
+        }]
+        },
+        {
+          name: "bob",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+          children: []
+        },
+        {
+          name: "Randy",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+          children: []
+        },
+        {
+          name: "Wanda",
+          gender: "female",
+          image: "images/p5.jpg",
+          info: "Born 1990",
+          children: []
+        },               
       ]
+      
     },
     {
         name: "John",
