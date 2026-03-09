414ebebd65c94a18049a72b8f288c5a4ba66d55b
Mia S <mia0613@gmail.com>
2026-03-08 21:03:50 -0500

    Update familyData.js

--- familyData.js
+++ familyData.js
@@ -5,24 +5,24 @@ const family =
 {
   name: "Anna's mother",
   gender: "female",
-  image: "images/p1.jpg",
+  image: "Images/p1.jpg",
   info: "Born 1940 • Founder of the dynasty",
   spouse: {
     name: "Anna's father",
     gender: "male",
-    image: "images/p2.jpg",
+    image: "Images/p2.jpg",
     info: "Born 1938 • Military leader"
   },
   children: [
     {
       name: "Anna",
       gender: "female",
-      image: "images/p3.jpg",
+      image: "Images/p3.jpg",
       info: "Born 1965",
       spouse: {
         name: "Carlos",
         gender: "male",
-        image: "images/p4.jpg",
+        image: "Images/p4.jpg",
         info: "Architect"
       },
       
@@ -31,40 +31,40 @@ const family =
         {
           name: "Wanda",
           gender: "female",
-          image: "images/Wanda.jpg",
+          image: "Images/Wanda.jpg",
           info: "Born 1921",
           spouse: {
             name: "Vladislav",
             gender: "male",
-            image: "images/pop.jpg",
+            image: "Images/pop.jpg",
             info: "Born 1912"
           },          
           children: [    
           {
           name: "Joan",
           gender: "female",
-          image: "images/Joan.png",
+          image: "Images/Joan.png",
           info: "Born 1949",
             spouse: {
             name: "Bill",
             gender: "male",
-            image: "images/Bill.png",
+            image: "Images/Bill.png",
             info: "Born 1947" },
           children: [      {
           name: "Valerie",
           gender: "female",
-          image: "images/Valerie.jpg",
+          image: "Images/Valerie.jpg",
           info: "Born 1977",
           children: [      {
               name: "Mia",
               gender: "female",
-              image: "images/Mia.png",
+              image: "Images/Mia.png",
               info: "Born 2005",
               children: []
             },       {
               name: "Ben",
               gender: "female",
-              image: "images/ben.png",
+              image: "Images/ben.png",
               info: "Born 2020",
               children: []
             }]
@@ -72,12 +72,12 @@ const family =
       {
           name: "Mike",
           gender: "female",
-          image: "images/mike.png",
+          image: "Images/mike.png",
           info: "Born 1979",
             spouse: {
             name: "Yuko",
             gender: "male",
-            image: "images/yuko.jpg",
+            image: "Images/yuko.jpg",
             info: "Born 1970 • Military leader"
           },
           children: [
@@ -87,12 +87,12 @@ const family =
               {
           name: "Peter",
           gender: "female",
-          image: "images/pete.jpg",
+          image: "Images/pete.jpg",
           info: "Born 1982",
             spouse: {
             name: "Mary",
             gender: "male",
-            image: "images/mary.jpg",
+            image: "Images/mary.jpg",
             info: "Born 1982 • Military leader"
           },
           children: []
@@ -102,14 +102,14 @@ const family =
       {
           name: "Sue",
           gender: "female",
-          image: "images/p5.jpg",
+          image: "Images/p5.jpg",
           info: "Born 1990",
           children: []
         },
 {
           name: "Dan",
           gender: "female",
-          image: "images/p5.jpg",
+          image: "Images/p5.jpg",
           info: "Born 1990",
           children: []
         }]
@@ -117,21 +117,21 @@ const family =
         {
           name: "bob",
           gender: "female",
-          image: "images/p5.jpg",
+          image: "Images/p5.jpg",
           info: "Born 1990",
           children: []
         },
         {
           name: "Randy",
           gender: "female",
-          image: "images/p5.jpg",
+          image: "Images/p5.jpg",
           info: "Born 1990",
           children: []
         },
         {
           name: "Wanda",
           gender: "female",
-          image: "images/p5.jpg",
+          image: "Images/p5.jpg",
           info: "Born 1990",
           children: []
         },               
@@ -141,12 +141,12 @@ const family =
     {
         name: "John",
         gender: "male",
-        image: "images/p6.jpg",
+        image: "Images/p6.jpg",
         info: "Born 1970",
         spouse: {
           name: "Sarah",
           gender: "female",
-          image: "images/p7.jpg",
+          image: "Images/p7.jpg",
           info: "Born 1972"
         }
       }
